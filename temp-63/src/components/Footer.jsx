e5c19be1a395e6def55cff6adc4afde36bfeb578
darshiyer <iamdarshiyer@gmail.com>
2025-02-14 21:45:32 +0530

    Add new component

---
+++ temp-63/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+// Footer component
+import React from "react";
+
+export const Footer = () => {
+  return <footer>© 2024</footer>;
+};
---
+++ temp-63/src/components/Header.jsx
@@ -0,0 +1,6 @@
+// Header component
+import React from "react";
+
+export const Header = () => {
+  return <header>Welcome</header>;
+};
